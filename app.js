b750d1ceae416551ed9ee83a3eb43891f0143e45
Ferxxo <garcilun02@gmail.com>
2025-07-27 18:01:47 -0600

    Se agregó nuevo proyecto.

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
-document.querySelectorAll('.semaforo').forEach(div => {
-    div.addEventListener('click', () => {
-       // Primero quitamos 'active' de todos
-        document.querySelectorAll('.semaforo').forEach(d => {
-            d.classList.remove('active');
-        });
-        // Luego activamos solo el clickeado
-        div.classList.add('active');
-        });
+document.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target); // Si solo quieres que se anime una vez
+      }
+    });
+  });
+
+  document.querySelectorAll('.reveal-on-scroll').forEach(el => observer.observe(el));
 });
+
+
+
+//CARRUSEL
+
+let currentIndex = 0;
+    let images = [];
+
+    function toggleDarkMode() {
+      document.documentElement.classList.toggle('dark');
+      document.body.classList.toggle('bg-white');
+      document.body.classList.toggle('text-gray-800');
+    }
+
+    function openCarousel(imgs) {
+      images = imgs;
+      currentIndex = 0;
+      document.getElementById('carouselImage').src = images[currentIndex];
+      document.getElementById('modal').classList.remove('hidden');
+      document.getElementById('modal').classList.add('flex');
+    }
+
+    function closeCarousel() {
+      document.getElementById('modal').classList.remove('flex');
+      document.getElementById('modal').classList.add('hidden');
+    }
+
+    function prevSlide() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      document.getElementById('carouselImage').src = images[currentIndex];
+    }
+
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % images.length;
+      document.getElementById('carouselImage').src = images[currentIndex];
+    }
